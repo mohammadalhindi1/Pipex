6da43e12d0c55c3d96a5c32e0ba5a18b3802cf75
Mohammad AlHindi <94603043+mohammadalhindi1@users.noreply.github.com>
2025-11-24 02:31:11 +0300

    Add files via upload

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: malhendi@student.42amman.com <malhendi>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 23:13:25 by malhendi          #+#    #+#             */
-/*   Updated: 2025/11/23 02:56:28 by malhendi@st      ###   ########.fr       */
+/*   Updated: 2025/11/24 01:47:05 by malhendi@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,17 +39,20 @@ typedef struct s_pipex
 	int		pipe_fd[2];
 }	t_pipex;
 
-void	free_2d(char **strings);
-void	free_cmds(char ***cmds, int count);
-void	cleanup_pipex(t_pipex *px);
 void	exit_handle(t_pipex *px, char *msg, int flag);
-
-void	init_paths(t_pipex *px);
-char	*get_cmd_path(t_pipex *px, char **cmd);
+void	free_cmds(char ***cmds, int count);
+void	free_2d(char **strings);
+void	cleanup_pipex(t_pipex *px);
 
 void	exec_child(t_pipex *px, int idx, int prev_fd);
+
+void	open_outfile(t_pipex *px);
+void	open_infile(t_pipex *px);
 void	pipex_run(t_pipex *px);
 
 void	handle_heredoc(t_pipex *px);
+
+void	init_paths(t_pipex *px);
+char	*get_cmd_path(t_pipex *px, char **cmd);
 
 #endif
